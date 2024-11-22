942e9fe0aff332c008192f21a284b736915f6fc0
Dmitry Gnatyshak <dmitry.gnatyshak@gmail.com>
2024-11-22 11:44:04 +0100

    Restructure `defaults` folder, add communications submodule

---
+++ mhagenta/defaults/communication/__init__.py
@@ -0,0 +1,4 @@
+from mhagenta.defaults.communication.communication import RestReceiver, RestSender, RMQReceiver, RMQSender
+
+
+__all__ = ['RestReceiver', 'RestSender', 'RMQReceiver', 'RMQSender']
---
+++ mhagenta/defaults/communication/communication.py
@@ -0,0 +1,17 @@
+from mhagenta.bases import PerceptorBase, ActuatorBase
+
+
+class RestReceiver(PerceptorBase):
+    pass
+
+
+class RestSender(ActuatorBase):
+    pass
+
+
+class RMQReceiver(PerceptorBase):
+    pass
+
+
+class RMQSender(ActuatorBase):
+    pass
