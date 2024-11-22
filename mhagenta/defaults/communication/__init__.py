7dab5f9ce5c2e807726990e0e6ca574c570d8787
Dmitry Gnatyshak <dmitry.gnatyshak@gmail.com>
2024-11-22 13:59:37 +0100

    Implement default REST-based receiver and sender (Perceptor and Actuator)

--- mhagenta/defaults/communication/__init__.py
+++ mhagenta/defaults/communication/__init__.py
@@ -1,4 +1,4 @@
-from mhagenta.defaults.communication.communication import RestReceiver, RestSender, RMQReceiver, RMQSender
+from mhagenta.defaults.communication.rest import RestReceiver, RestSender
 
 
-__all__ = ['RestReceiver', 'RestSender', 'RMQReceiver', 'RMQSender']
+__all__ = ['RestReceiver', 'RestSender']
