121ef16a2d7ee14a870458cd5cd53609ee427b03
Dmitry Gnatyshak <dmitry.gnatyshak@gmail.com>
2025-03-06 14:36:24 +0100

    Add DEFAULT_RMQ_IMAGE

--- mhagenta/utils/__init__.py
+++ mhagenta/utils/__init__.py
@@ -2,11 +2,12 @@ from mhagenta.utils.common import State, Belief, Goal, Observation, ActionStatus
 
 
 DEFAULT_PORT = 61200
+DEFAULT_RMQ_IMAGE = 'rabbitmq:4.0-management'
 
 
 __all__ = [
     'Outbox',
     'ConnType', 'Message', 'AgentCmd', 'StatusReport',
     'State', 'Belief', 'Goal', 'Observation', 'ActionStatus', 'ModuleTypes', 'ModuleTypes', 'LoggerExtras', 'ILogging',
-    'Directory', 'Performatives', 'DEFAULT_PORT'
+    'Directory', 'Performatives', 'DEFAULT_PORT', 'DEFAULT_RMQ_IMAGE'
 ]
